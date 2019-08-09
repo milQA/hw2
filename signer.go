f3efd93975446be9beded38313ea2dac2c6cc63e
Nur Kutlugallyamov <47988791+milQA@users.noreply.github.com>
2019-08-09 10:35:23 +0500

    Update signer.go

--- signer.go
+++ signer.go
@@ -5,33 +5,41 @@ import (
 	"strconv"
 	"sync"
 	"time"
+	"runtime"
+	"fmt"
+	"sync/atomic"
 )
 
 // сюда писать код
 
-// доделать нужно
-func ExecutePipeline() {
+// доделано
+func ExecutePipeline(in []string) {
 	//конвейерную обработку функций-воркеров
 	var results []string
 	mu := &sync.Mutex{} //нужны чтобы md5 чекать
 	ch1 := make(chan string, 1)
-
-	for i := 0; i < 2; i++ {
-		go func(i int, mu *sync.Mutex, ch1 chan<- string) {
+	inCount := len(in)
+	for _, i := range in {
+		go func(i string, mu *sync.Mutex, ch1 chan<- string) {
 			wg := &sync.WaitGroup{}
 			wg.Add(1)
-			data := strconv.Itoa(i)
-			ansSingleHash := SingleHash(data, wg, mu)
+			//data := strconv.Itoa(i)
+			ansSingleHash := SingleHash(i /*data,*/, wg, mu)
 			wg.Wait()
 			ch1 <- MultiHash(ansSingleHash)
 		}(i, mu, ch1)
 	}
-	results = append(results, <-ch1)
-	results = append(results, <-ch1)
+	for i := 0; i < inCount; i++ {
+		results = append(results, <-ch1)
+	}
 	answer := CombineResults(results) //вывод конечного результата
+	//time.Sleep(5*time.Second)
+	fmt.Println("=====================")
+	fmt.Println("EP:")
+	fmt.Println(answer)
 }
 
-// готово, вроде как
+// готово
 func SingleHash(data string, wg *sync.WaitGroup, mu *sync.Mutex) string {
 	defer wg.Done()
 
@@ -39,64 +47,76 @@ func SingleHash(data string, wg *sync.WaitGroup, mu *sync.Mutex) string {
 	ch2 := make(chan string)
 	ch3 := make(chan string)
 
-	go crc32(ch2, data)
+	go fcrc32(ch2, data)
 
 	go func(ch1 chan<- string, data string, mu *sync.Mutex) {
-		ch1 <- md5(data, mu)
+		ch1 <- fmd5(data, mu)
 	}(ch1, data, mu)
 
-	go func(ch1 <-chan string) {
+	go func(ch3 chan<- string, ch1 <-chan string) {
 		md5data := <-ch1
-		crc32(ch3, md5data)
-	}(ch1)
+		fcrc32(ch3, md5data)
+	}(ch3, ch1)
 
 	result1 := <-ch2
 	result2 := <-ch3
 	result := result1 + "~" + result2
+	fmt.Println(result) //norm. без перегревов
 	return result
 	//crc32(data)+"~"+crc32(md5(data))
 
 }
 
-//переделать нужно
+//сделано. Выводит MH result
 func MultiHash(data string) string {
 	var answer []string
-	wg := &sync.WaitGroup{}
+	ch1 := make(chan string)
+	//wg := &sync.WaitGroup{}
 	for th := 0; th < 6; th++ {
-		wg.Add(1)
-		go func(wg *sync.WaitGroup, th int, data string, answer []string) {
-			defer wg.Done()
-			answer = append(answer, crc32(strconv.Itoa(th)+data)) // здесь
-		}(wg, th, data, answer)
+		//wg.Add(1)
+		go func( /*wg *sync.WaitGroup,*/ th int, data string) {
+			//defer wg.Done()
+			fcrc32(ch1, strconv.Itoa(th)+data) // здесь
+			runtime.Gosched()
+		}( /*wg,*/ th, data)
 	}
-	time.Sleep(time.Millisecond)
-	wg.Wait()
+	for th := 0; th < 6; th++ {
+		answer = append(answer, <-ch1)
+		fmt.Println(th, "||", answer[th])
+	}
+	//time.Sleep(time.Millisecond)
+	//wg.Wait() //ожидание, что все MH отработают
 	ans := ""
 	for _, value := range answer {
 		ans = ans + value
 	}
+	//fmt.Println(ans)
 	return ans
 }
 
 func CombineResults(data []string) string {
+	fmt.Println("=====")
+	fmt.Println(data)
+	fmt.Println("=====")
 	sort.Strings(data)
-	answer := ""
-	for _, str := range data {
-		answer := answer + "_" + str
+	answer := data[0]
+	for i := 1; i < len(data); i++ {
+		answer = answer + "_" + data[i]
 	}
 	return answer
 }
 
-func crc32(ch chan<- string, data string) {
-	ch <- DataSignerCrc32(data)
+func fcrc32(ch chan<- string, data string) {
+	temp := DataSignerCrc32(data)
+	//fmt.Println("crc32 ",temp)
+	ch <- temp
 }
 
-func md5(data string, mu *sync.Mutex) string {
+func fmd5(data string, mu *sync.Mutex) string {
 	mu.Lock()
 	defer mu.Unlock()
 	return DataSignerMd5(data)
 }
-
 /*
 0 SingleHash data 0
 0 SingleHash md5(data) cfcd208495d565ef66e7dff9f98764da
@@ -110,7 +130,6 @@ func md5(data string, mu *sync.Mutex) string {
 4108050209~502633748 MultiHash: crc32(th+step1)) 4 2730963093
 4108050209~502633748 MultiHash: crc32(th+step1)) 5 1025356555
 4108050209~502633748 MultiHash result: 29568666068035183841425683795340791879727309630931025356555
-
 1 SingleHash data 1
 1 SingleHash md5(data) c4ca4238a0b923820dcc509a6f75849b
 1 SingleHash crc32(md5(data)) 709660146
@@ -123,6 +142,5 @@ func md5(data string, mu *sync.Mutex) string {
 2212294583~709660146 MultiHash: crc32(th+step1)) 4 259286200
 2212294583~709660146 MultiHash: crc32(th+step1)) 5 2427381542
 2212294583~709660146 MultiHash result: 4958044192186797981418233587017209679042592862002427381542
-
 CombineResults 29568666068035183841425683795340791879727309630931025356555_4958044192186797981418233587017209679042592862002427381542
 */
